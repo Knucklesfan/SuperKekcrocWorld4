aa399343355d6529e7ffc6c3bdca81ff2ae16c8e
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2022-02-03 13:33:19 -0600

    player implementation

--- BirdPlumberEngine/source/player.h
+++ BirdPlumberEngine/source/player.h
@@ -4,22 +4,37 @@
 #include <cute_c2.h>
 #include "block.h"
 
+#define PLAYER_HEIGHT 32.0f
+#define PLAYER_HALF_WIDTH 8.0f
+#include <cute_math2d.h>
+
 class Player
 {
 	public:
 		int type = object::PLAYER;
-		double x;
-		double y;
+		v2 position;
+		v2 velocity;
+		bool grounded;
+		bool jumpable;
+
 		SDL_Texture* sprite;
 		void render(SDL_Renderer* render, int viewx, int viewy);
 		void logic(double deltaTime);
 		void physics(double deltatime, std::vector<block*> colliders, int width, std::vector<GameObject*> objects);
 		Player(int xa, int ya, SDL_Texture* sheet, SDL_Renderer* render);
+		void player_sync_geometry();
 		int getx();
 		int gety();
 		int gettype();
-		double xvelocity;
-		double yvelocity;
-		c2AABB collider;
+		aabb_t collider;
+		c2Capsule capsule;
+		aabb_t c2(c2AABB box) { return make_aabb(c2(box.min), c2(box.max)); }
+		c2v c2(v2 v) { return c2V(v.x, v.y);}
+		v2 c2(c2v v) { return v2(v.x, v.y); }
+		float player_sweep(c2Capsule capsule, v2* n, v2* contact, v2 vel,std::vector<block*> colliders, int width);
+		void player_ngs(std::vector<block*> colliders, int width);
+		void player_sync_geometry (Player* player);
+		int player_can_fall(int pixels_to_fall,std::vector<block*> colliders, int width);
+		C2_TYPE tileconv(int id);
 };
 
